a99e8ecafd68b7d5b893b0bd088a7a0dd53462e7
amjad <amjad.mousa.600@gmail.com>
2025-01-22 11:46:50 +0200

    Add Product

---
+++ FirstEx/Inventory.cs
@@ -0,0 +1,9 @@
+using 
+public class Inventory
+{
+   private List<Product> products = new List<Product>();
+   public void AddProduct(Product)
+   {
+      products.Add(product);
+   }
+}
---
+++ FirstEx/Product.cs
@@ -0,0 +1,12 @@
+namespace MyNamespace;
+
+public class Product(string name, double price, int quantity)
+{
+    public string Name { get; set; } = name;
+    public double Price { get; set; } = price;
+    public int Quantity { get; set; } = quantity;
+
+    public Product() : this(string.Empty, 0, 0)
+    {
+    }
+}
