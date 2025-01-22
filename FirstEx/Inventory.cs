c653257452b574755ea2c7ed8446817f4fbc3751
amjad <amjad.mousa.600@gmail.com>
2025-01-22 12:31:52 +0200

    AddProduct method

--- FirstEx/Inventory.cs
+++ FirstEx/Inventory.cs
@@ -1,66 +1,71 @@
-namespace ConsoleApp1;
-public class Inventory
-{
-private List<Product> products=new List<Product>();
-
-public void AddProduct(Product product)
-{
-    products.Add(product);
-}
-
-public void ShowProducts()
-{
-    if (products.Count==0)
+    namespace ConsoleApp1;
+    public class Inventory
     {
-        System.Console.WriteLine("There are no products in inventory.");
-    }
+    private List<Product> products=new List<Product>();
 
-    foreach (var product in products)
+    public void AddProduct(Product product)
     {
-        System.Console.WriteLine($"ProductName:{products[0].Name}, Price:{products[0].Price}, Quantity:{products[0].Quantity}");
-    }
-    
-}
-
-public bool FindProduct(string productName)
-{
-    foreach (var product in products)
-    {
-        if (product.Name == productName)
+        if (FindProduct(product.Name))
         {
-            return true;
+            Product p = products.Find(x => x.Name == product.Name);
+            p.Quantity += product.Quantity;
         }
+        else products.Add(product);
     }
-    return false;
- }
 
-public string EditProduct(string productName, string productPrice, string productQuantity)
-{
-    if (FindProduct(productName))
+    public void ShowAllProducts()
     {
-        productPrice = productPrice;
-        productQuantity = productQuantity;
-        return "Product updated successfully.";
-    }
-    else return "Product not found.";
-}
+        if (products.Count==0)
+        {
+            System.Console.WriteLine("There are no products in inventory.");
+        }
 
-public string DeleteProduct(string productName)
-{
-    if (FindProduct(productName))
-    {
-        products.Remove(products.First(x=>x.Name == productName));
-        return "Product deleted successfully.";
+        foreach (var product in products)
+        {
+            System.Console.WriteLine($"ProductName:{products[0].Name}, Price:{products[0].Price}, Quantity:{products[0].Quantity}");
+        }
+        
     }
-    else return "Product not found.";
-}
 
-public string SearchProduct(string productName)
-{
-    if (FindProduct(productName))
+    public bool FindProduct(string productName)
     {
-        return $"Product found: {products.First(x => x.Name == productName).Name},Price:{products.First(x=>x.Name==productName)},Quantity: {products.First(x => x.Name == productName).Quantity}";
+        foreach (var product in products)
+        {
+            if (product.Name == productName)
+            {
+                return true;
+            }
+        }
+        return false;
+     }
+
+    public string EditProduct(string productName, string productPrice, string productQuantity)
+    {
+        if (FindProduct(productName))
+        {
+            productPrice = productPrice;
+            productQuantity = productQuantity;
+            return "Product updated successfully.";
+        }
+        else return "Product not found.";
     }
-    else return "Product not found.";
-}
-}
+
+    public string DeleteProduct(string productName)
+    {
+        if (FindProduct(productName))
+        {
+            products.Remove(products.First(x=>x.Name == productName));
+            return "Product deleted successfully.";
+        }
+        else return "Product not found.";
+    }
+
+    public string SearchProduct(string productName)
+    {
+        if (FindProduct(productName))
+        {
+            return $"Product found: {products.First(x => x.Name == productName).Name},Price:{products.First(x=>x.Name==productName)},Quantity: {products.First(x => x.Name == productName).Quantity}";
+        }
+        else return "Product not found.";
+    }
+    }
